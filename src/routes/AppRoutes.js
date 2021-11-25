b5b4518a1a5ae76019d9a9d68e23b7e09f8971f2
chicodevbr <chicodevbr@gmail.com>
2021-11-25 18:12:12 -0300

    default page

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,14 +2,16 @@ import { Home } from 'grommet-icons';
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import AddAlbum from '../components/AddAlbum/AddAlbum';
+import AlbumsList from '../components/Albums/AlbumsList';
 import Album from '../Pages/Album';
 import AllAlbums from '../Pages/AllAlbums';
+import DefaultPage from '../templates/DefaultPage';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={AlbumsList} />
         <Route path="/album/:albumId" component={Album} />
         <Route path="/albums" component={AllAlbums} />
         <Route path="/add" component={AddAlbum} />
