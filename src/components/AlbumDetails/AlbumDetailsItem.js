65b6c353dadbbedcc1f5fb691c49a8bdcaa0234a
chicodevbr <chicodevbr@gmail.com>
2021-11-26 21:18:19 -0300

    change title name

--- src/components/AlbumDetails/AlbumDetailsItem.js
+++ src/components/AlbumDetails/AlbumDetailsItem.js
@@ -4,15 +4,16 @@ import { Box, Heading, Image, Main, Text } from '../UI/Components';
 const AlbumDetailsItem = (props) => {
   return (
     <Box align="start">
-      <Heading>{props.name}</Heading>
+      <Heading>{props.artist}</Heading>
       <Main direction="row">
         <Box height="small" width="small">
           <Image src={props.image} atl={props.name} fit="cover" />
         </Box>
         <Box direction="column" pad="small">
-          <Text size={'2xl'}>{props.artist}</Text>
+          <Text size={'2xl'}>{props.name}</Text>
           <Text size={'medium'}>Release: {props.year}</Text>
           <Text size={'medium'}>Producer: {props.producer}</Text>
+          <Text size={'medium'}>Label: {props.label}</Text>
         </Box>
       </Main>
     </Box>
