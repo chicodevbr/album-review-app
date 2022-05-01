4e2af5e7465f95b828e4bf353c6abc0671cb5663
chicodevbr <chicodevbr@gmail.com>
2022-05-01 10:59:02 -0300

    :lipstick: feat: estilizando profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,7 +1,34 @@
+import { Avatar, Box, Heading, Paragraph } from 'grommet';
 import DefaultPage from '../templates/DefaultPage';
 
 const Profile = () => {
-  return <DefaultPage>Profile</DefaultPage>;
+  const src =
+    'https://images.unsplash.com/photo-1519345997025-14aba50d7e9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80';
+  return (
+    <DefaultPage>
+      <Box>
+        <Box gap="small">
+          <Avatar size="3xl" src={src} />
+          <Heading>Julia Andrews</Heading>
+        </Box>
+        <Box>
+          <Heading level={3} size="small" pad="small">
+            About
+          </Heading>
+          <Paragraph>
+            Julia Andrews is Head of Product at YatLabs and is responsible for
+            leading several teams.
+          </Paragraph>
+        </Box>
+        <Box>
+          <Heading level={3} size="small" pad="small">
+            Reviews
+          </Heading>
+          <Paragraph>List de Reviews</Paragraph>
+        </Box>
+      </Box>
+    </DefaultPage>
+  );
 };
 
 export default Profile;
